9fa915c31b12d2c9a9741017bd310162fd901fea
Fl0-dev <florian@lamacompta.co>
2024-03-18 09:41:35 +0100

    feat: add endpoint for create 100 users

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,6 +53,7 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
+$route['seed'] = 'seeder/seed';
 
 /************ API ******************/
 $route['api/users/register'] = 'api/user/register';
